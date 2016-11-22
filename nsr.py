3b497de9de18b60541f17e63c13b9264ff5a0d53
Vitor Hugo <vtr@hotmail.com.br>
2016-11-22 16:05:05 -0200

    Added CACHE render | Added current take correction

--- nsr.py
+++ nsr.py
@@ -8,10 +8,12 @@ import ConfigParser as CP
 '''
 Takes need to be set inside Houdini
 
-output format must be:
+
+output_format:
     PROJECT_NAME.ROP_NODE_NAME.FRAME.EXTENSION
 
-READ CACHE FILES MUST HAVE FULL PATH
+Cache files must have full path
+
 
 
 
@@ -21,10 +23,11 @@ READ CACHE FILES MUST HAVE FULL PATH
 conf = CP.ConfigParser()
 conf.read('toRender.cfg')
 HOUDINI_FOLDER = "E:\Videos\Houdini"
+CHOUDINI_FOLDER = "C:\Houdini"
 
 
 def get_last_frame(project_directory, END_FRAME, rop_node):
-    _ = glob.glob("{}\\*.{}.*.*".format(project_directory, rop_node))
+    _ = glob.glob("{}\\*.{}.*".format(project_directory, rop_node))
     if not _:
         return 1  # nothing was rendered yet
     frame_nums = sorted([int(file.split('.')[2]) for file in _])
@@ -34,20 +37,23 @@ def get_last_frame(project_directory, END_FRAME, rop_node):
     return int(frame)
 
 
-def render(HOUDINI_FOLDER, PROJECT_NAME, HIPFILE, frame, extension, rop_node="mantra1"):
+def render(PROJECT_NAME, RENDER_FOLDER, HIPFILE, frame, extension, rop_node="mantra1"):
     try:
         hou.hipFile.load(HIPFILE)
     except hou.OperationFailed as e:
         return "Not possible to load file {} - {}".format(HIPFILE, e)
     render_node = hou.node("/out/{}".format(rop_node))
     operator_name = render_node.name()
+    # Defines main take so it can set the frame range
+    main_take = hou.takes.takes()[-1]
+    if hou.takes.currentTake().name() is not "Main":
+        hou.takes.setCurrentTake(main_take)
     render_node.parm('trange').set(1)
 
     if extension is None:
         extension = render_node.parm("vm_device").eval()
-
-    path = "{}\\{}\\render\{}.{}.{}.{}".format(
-        HOUDINI_FOLDER, PROJECT_NAME, PROJECT_NAME, operator_name, frame, extension)
+    path = "{}\\{}.{}.{}.{}".format(
+        RENDER_FOLDER, PROJECT_NAME, operator_name, frame, extension)
 
     render_node.render(frame_range=(frame, frame),
                        output_file=path, verbose=True, output_progress=False)
@@ -55,7 +61,10 @@ def render(HOUDINI_FOLDER, PROJECT_NAME, HIPFILE, frame, extension, rop_node="ma
 
 def main():
     for project in conf.sections():
-        PROJECT_NAME = conf.get(project, 'project_name')
+        try:
+            PROJECT_NAME = conf.get(project, 'project_name')
+        except CP.NoOptionError:
+            raise "No project name"
 
         try:
             HIP_EXTENSION = conf.get(project, 'hip_extension')
@@ -68,26 +77,49 @@ def main():
             OUT_EXTENSION = None  # Uses default defined in the ROP node, defined in func render
 
         try:
+            CACHE_EXTENSION = conf.get(project, 'cache_extension')
+        except CP.NoOptionError:
+            CACHE_EXTENSION = 'bgeo.sc'
+
+        try:
             ROP_NODES = [node.strip()
                          for node in conf.get(project, 'rop_nodes').split(',')]
         except CP.NoOptionError:
             # Uses default defined in the ROP node, defined in func render
             ROP_NODES = ['mantra1']
 
-        RENDER_FOLDER = "{}\\{}\\render".format(HOUDINI_FOLDER, PROJECT_NAME)
+        try:
+            CACHE_NODES = [node.strip()
+                           for node in conf.get(project, 'cache_nodes').split(',')]
+        except CP.NoOptionError:
+            CACHE_NODES = []
 
         HIPFILE = "{}\\{}\\{}.{}".format(
             HOUDINI_FOLDER, PROJECT_NAME, PROJECT_NAME, HIP_EXTENSION)
 
+        RENDER_FOLDER = "{}\\{}\\render".format(HOUDINI_FOLDER, PROJECT_NAME)
+
         END_FRAME = int(conf.get(project, 'end_frame'))
+
+        for cache_node in CACHE_NODES:
+            CCACHE = "{}\\{}\\{}".format(
+                CHOUDINI_FOLDER, PROJECT_NAME, cache_node)
+            START_FRAME = get_last_frame(CCACHE, END_FRAME, cache_node)
+            if START_FRAME:
+                for frame in range(START_FRAME, END_FRAME + 1):
+                    render(PROJECT_NAME, CCACHE, HIPFILE,
+                           frame, CACHE_EXTENSION, cache_node)
+            else:
+                print("Nothing to render in {} from {}!".format(
+                    cache_node, project))
 
         for rop_node in ROP_NODES:
             START_FRAME = get_last_frame(RENDER_FOLDER, END_FRAME, rop_node)
             if START_FRAME:
                 for frame in range(START_FRAME, END_FRAME + 1):
-                    render(HOUDINI_FOLDER, PROJECT_NAME,
-                           HIPFILE, frame, OUT_EXTENSION, rop_node)
-
+                    render(PROJECT_NAME, RENDER_FOLDER, HIPFILE, frame,
+                           OUT_EXTENSION, rop_node)
+                    pass
             else:
                 print("Nothing to render in {} from {}!".format(rop_node, project))
 
