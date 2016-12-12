ab4557025d93ccb14b79b0772eeb9c0851e85cbf
Vitor Hugo <vtr@hotmail.com.br>
2016-12-12 11:18:59 -0200

    implemented per node end frame

--- populate_toRender.py
+++ populate_toRender.py
@@ -5,19 +5,24 @@ import ConfigParser
 def get_rop_nodes_list(out_node):
     rop_nodes = []
     cache_nodes = []
+    render_nodes = {"rop_nodes": [], "cache_nodes": []}
     for child in out_node.children():
         # node is set to current frame, so we skip it
+        end_frame = child.parm("f2").eval()
         if child.parm("trange").eval() == 0:
             pass
         elif any(word in child.name().lower() for word in ["control", "preview", "single"]):
             pass
         elif "CACHE" in child.name().upper():
+            render_nodes['cache_nodes'].append((child.name(), end_frame))
             cache_nodes.append(child.name())
         else:
+            render_nodes['rop_nodes'].append((child.name(), end_frame))
             rop_nodes.append(child.name())
     cache_nodes = str(cache_nodes).strip("[]").translate(None, "'")
     rop_nodes = str(rop_nodes).strip("[]").translate(None, "'")
-    return cache_nodes, rop_nodes
+    # return cache_nodes, rop_nodes
+    return render_nodes
 
 
 def get_max_frame(out_node):
@@ -44,10 +49,12 @@ def get_output_extension(out_node):
     rop_extension = None
     for child in out_node.children():
         if "CONTROL" not in child.name().upper():
+            #It's a ROP node
             try:
                 output_file = child.parm("vm_picture").eval()
                 rop_extension = output_file.split("\\")[-1].split(".")[-1]
             except AttributeError:
+            #It's a CACHE node
                 try:
                     output_file = child.parm("sopoutput").eval()
                     file_extension = output_file.split("\\")[-1].split(".")[-1]
@@ -60,16 +67,15 @@ def get_output_extension(out_node):
     return cache_extension, rop_extension
 
 
+
 def write(config_file, out):
     config_handle = ConfigParser.RawConfigParser()
     HIPNAME = hou.expandString("$HIPNAME").split("\\")[-1]
-    cache_nodes = get_rop_nodes_list(out)[0]
+    cache_nodes = get_rop_nodes_list(out)["cache_nodes"]
     config_handle.add_section("{}".format(HIPNAME))
     config_handle.set('{}'.format(HIPNAME), 'project_name', HIPNAME)
-    config_handle.set('{}'.format(HIPNAME),
-                      'end_frame', get_max_frame(out))
     config_handle.set('{}'.format(HIPNAME), 'rop_nodes',
-                      get_rop_nodes_list(out)[1])
+                      get_rop_nodes_list(out)["rop_nodes"])
     config_handle.set('{}'.format(HIPNAME), 'rop_extension',
                       get_output_extension(out)[1])
     if cache_nodes:
@@ -78,6 +84,7 @@ def write(config_file, out):
                           get_output_extension(out)[0])
     with open(config_file, 'a') as configfile:
         config_handle.write(configfile)
+    print(get_rop_nodes_list(out))
 
 
 def check_projects(config_file, project_name):
